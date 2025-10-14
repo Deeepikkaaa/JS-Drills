4aba054890deec64a99dd7e156b3c0db927c3b73
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-10-14 17:26:37 +0545

    Encoding characters using #

--- mask.js
+++ mask.js
@@ -4,3 +4,7 @@ const str="123456789111213141516";
 const maskChar='#'.repeat(5);
 const result=str.slice(0,str.length-5)+maskChar;
 console.log(result);
+
+for(let i=0;i<=str.length-5;i--){
+    
+}
