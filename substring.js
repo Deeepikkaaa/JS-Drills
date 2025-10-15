b8c98f534ca16fb4797c9123501e497123c2d574
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-10-15 09:40:18 +0545

    Substing of given string

---
+++ substring.js
@@ -0,0 +1,9 @@
+//Substring in js without using substring() method
+//getting the substring of the string mycountryNepal
+
+let string="mycountryNepal";
+for(let i=0;i<1;i++){
+    for (let j=0;j<=string.length;j++){
+        console.log(string.slice(i,j))
+    }
+}
